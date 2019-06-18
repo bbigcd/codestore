d055a3710338621b45acf3c0deeb224f6de084b7
陈弟CD <bbigcd@163.com>
2019-06-18 16:19:26 +0800

    添加vue模块handsontable工具测试

--- src/vue/handsontable/src/components/HelloWorld.vue
+++ src/vue/handsontable/src/components/HelloWorld.vue
@@ -1,58 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button @click="submit">打印</button>
+    <hot-table :settings="settings"/>
+    <br>
+    <hot-table :data="data" :rowHeaders="true" :colHeaders="true" licenseKey='non-commercial-and-evaluation'></hot-table>
   </div>
 </template>
 
 <script>
+import { HotTable } from "@handsontable/vue";
+import "handsontable/dist/handsontable.full.css";
+import Handsontable from "handsontable";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    HotTable
+  },
+  data: function() {
+    return {
+      data: [
+        ["年份\\品牌", "Tesla", "Mercedes", "Toyota", "Volvo"],
+        ["2019", 10, 11, 12, 13],
+        ["2020", 20, 11, 14, 13],
+        ["2021", 30, 15, 12, 13]
+      ],
+      settings: {
+        data: Handsontable.helper.createSpreadsheetData(6, 10),
+        rowHeaders: true,
+        colHeaders: true,
+        licenseKey: "non-commercial-and-evaluation"
+      }
+    }
+  },
+  methods: {
+    submit() {
+      // eslint-disable-next-line no-console
+      console.log(this.settings)
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
