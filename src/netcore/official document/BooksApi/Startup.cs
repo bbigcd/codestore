2a3102ec546cfba1cb9483b84e9f518fbdc35adf
陈弟CD <bbigcd@163.com>
2019-06-28 17:46:58 +0800

    添加“使用 MongoDB 的 Web API” 代码

--- src/netcore/official document/BooksApi/Startup.cs
+++ src/netcore/official document/BooksApi/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksApi.Models;
+using BooksApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,20 @@ namespace BooksApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /* appsettings.json 文件的 BookstoreDatabaseSettings 
+               部分绑定到的配置实例在依赖关系注入 (DI) 容器中注册 。
+               例如，BookstoreDatabaseSettings 对象的 ConnectionString 
+               属性使用 appsettings.json 中的 BookstoreDatabaseSettings:ConnectionString 属性进行填充 。
+             */
+            services.Configure<BookstoreDatabaseSettings>(Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+            /*
+               IBookstoreDatabaseSettings 接口使用单一实例服务生存期在 DI 中注册。
+               在注入时，接口实例时将解析为 BookstoreDatabaseSettings 对象。
+             */
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp => sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+
+            services.AddSingleton<BookService>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
